8b42b53db4a36c8b997ec4d17ee901a9d7cc87e4
EGOIST <0x142857@gmail.com>
2017-03-31 16:48:32 +0800

    optimize updater

--- src/pwa.js
+++ src/pwa.js
@@ -1,50 +1,18 @@
-import Vue from 'vue'
 import runtime from 'offline-plugin/runtime'
-import { Notification, Button } from 'element-ui'
-
-const createNotifier = () => new Vue({
-  data: {
-    updating: false
-  },
-  created() {
-    const h = this.$createElement
-    this.notification = Notification({ // eslint-disable-line new-cap
-      title: 'Detected an update for CodePan, apply updates will reload the tab automatically!',
-      type: 'info',
-      message: h('el-button', {
-        on: { click: this.update },
-        props: {
-          icon: 'check',
-          type: 'success',
-          loading: this.updating
-        }
-      }, [this.updating ? 'Updating' : 'Apply Updates']),
-      duration: 0
-    })
-  },
-  methods: {
-    update() {
-      this.updating = true
-      runtime.applyUpdate()
-    }
-  },
-  components: {
-    'el-button': Button
-  }
-})
-
-let notifier
+import { Notification } from 'element-ui'
 
 runtime.install({
   onUpdateReady() {
     console.log('update ready')
-    notifier = createNotifier()
+    runtime.applyUpdate()
   },
 
   onUpdated() {
-    console.log('updated')
-    notifier.updating = false
-    notifier.notification.close()
-    location.reload()
+    Notification({ // eslint-disable-line new-cap
+      title: 'New version is ready',
+      type: 'info',
+      message: 'CodePan has been updated in the background, you can refresh the page to apply updates!',
+      duration: 8000
+    })
   }
 })
