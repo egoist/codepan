0afcd784169a77d8e1136c6e98122432369542a9
EGOIST <0x142857@gmail.com>
2017-04-16 18:26:17 +0800

    use vbuild 7

--- vbuild.config.js
+++ vbuild.config.js
@@ -1,34 +1,28 @@
 const OfflinePlugin = require('offline-plugin')
 
 module.exports = options => ({
-  entry: 'src/index.js',
   postcss: [
     // add more postcss plugins here
     // by default we have autoprefixer pre added
     require('postcss-nested'),
     require('postcss-property-lookup')
   ],
-  webpack(config) {
+  extendWebpack(config) {
     // inject offline-plugin in production build
-    if (!options.dev) {
-      config.plugins.push(new OfflinePlugin({
-        caches: {
-          main: [':rest:']
-        },
-        ServiceWorker: {
-          events: true
-        },
-        AppCache: {
-          events: true
-        }
-      }))
+    if (options.mode === 'production') {
+      config.plugin('offline')
+        .use(OfflinePlugin, [{
+          caches: {
+            main: [':rest:']
+          },
+          ServiceWorker: {
+            events: true
+          },
+          AppCache: {
+            events: true
+          }
+        }])
     }
-
-    config.node = {
-      fs: 'empty'
-    }
-
-    return config
   },
   production: {
     sourceMap: false
