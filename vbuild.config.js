6fba2c7f6b22c2dc02f2c3def11032c6f96282a9
EGOIST <0x142857@gmail.com>
2017-04-17 21:05:49 +0800

    set postcss in package.json

--- vbuild.config.js
+++ vbuild.config.js
@@ -1,12 +1,6 @@
 const OfflinePlugin = require('offline-plugin')
 
 module.exports = options => ({
-  postcss: [
-    // add more postcss plugins here
-    // by default we have autoprefixer pre added
-    require('postcss-nested'),
-    require('postcss-property-lookup')
-  ],
   extendWebpack(config) {
     // inject offline-plugin in production build
     if (options.mode === 'production') {
