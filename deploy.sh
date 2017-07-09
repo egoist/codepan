52c310b59b52e347ddb4e6d1d7b34b8761e89d48
EGOIST <0x142857@gmail.com>
2017-07-09 22:30:54 +0800

    tweak deploy script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [ "$CI" = "true" ]
+then
+  git config --global user.email "bot@egoist.moe"
+  git config --global user.name "egoist bot"
+  repo=https://$GITHUB_TOKEN@github.com/egoist/codepan.git
+else
+  repo=git@github.com:egoist/codepan.git
+fi
+
+./node_modules/.bin/gh-pages \
+  --silent $CI \
+  --repo $repo \
+  --dist dist
