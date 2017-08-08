abe800f76f59f7a03f19abf4fd7f2b072a115e21
EGOIST <0x142857@gmail.com>
2017-08-08 10:29:33 +0800

    Cache Babel

--- poi.config.js
+++ poi.config.js
@@ -1,5 +1,7 @@
 const nodeModules = require('webpack-node-modules')
 
+const BABEL_CDN = 'https://cdn.jsdelivr.net/npm/babel-standalone@7.0.0-alpha.15/babel.min.js'
+
 module.exports = {
   extendWebpack(config) {
     config.module.noParse
@@ -15,6 +17,9 @@ module.exports = {
     sourceMap: false
   },
   homepage: '/',
+  env: {
+    BABEL_CDN
+  },
   presets: [
     require('poi-preset-bundle-report')(),
     require('poi-preset-offline')({
@@ -37,7 +42,8 @@ module.exports = {
         },
         externals: [
           'https://reasonml.github.io/bs.js',
-          'https://reasonml.github.io/refmt.js'
+          'https://reasonml.github.io/refmt.js',
+          BABEL_CDN
         ]
       }
     })
