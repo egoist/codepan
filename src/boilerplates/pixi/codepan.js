3ad33c79a757e0a615d748cd7f24f39025971abb
Jacek Pietal <jacek@gamecodehq.com>
2019-06-23 21:37:22 +0200

    codepan-pixi-update
    (cherry picked from commit 471f003a984fcfca6b5acbdfd0e872a74f1516d0)

--- src/boilerplates/pixi/codepan.js
+++ src/boilerplates/pixi/codepan.js
@@ -4,11 +4,14 @@ const app = createApp()
 function createSprite() {
   const sprite = PIXI.Sprite.from('https://pixijs.io/examples/examples/assets/bunny.png')
   sprite.anchor.set(0.5)
+  sprite.scale.set(3)
   return sprite
 }
 
 function createApp() {
-  return new PIXI.Tiled.FullscreenApplication(tick)
+  return new PIXI.Tiled.FullscreenApplication(tick, {
+    backgroundColor: 0xffffff
+  })
 }
 
 function tick(time) {
