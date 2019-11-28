b1934f3aee70fa6103dd79d255523c3dc9925efa
Michael Rubanov <mrubanov@gmail.com>
2019-11-29 01:42:48 +0200

    p5.js default sketch

---
+++ src/boilerplates/p5/codepan.js
@@ -0,0 +1,11 @@
+const cl = console.log;
+const W = H = 400;
+function setup() {
+  createCanvas(W, H);
+  background(0);
+}
+function draw() {
+  const middle = W/2;
+  const diameter = 10;
+  circle(middle,middle,diameter);
+}
